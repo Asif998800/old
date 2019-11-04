1a06a043320892d55810f5453cb9b347c6e45007
Asif998800 <asif018600@gmail.com>
2019-11-04 06:01:51 -0600

    add categories

--- src/article/models.py
+++ src/article/models.py
@@ -2,9 +2,16 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
     title = models.CharField(max_length=30)
+    category = models.ManyToManyField(Category)
     content = models.TextField()
     date = models.DateTimeField(auto_now=True)
 
