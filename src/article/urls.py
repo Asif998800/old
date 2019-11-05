750f6fe615a4cb7351c6e2b7bd6dba1b7baf80ef
Asif998800 <asif018600@gmail.com>
2019-11-05 05:25:36 -0600

    Added CRUD function

--- src/article/urls.py
+++ src/article/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('article/<pk>/', views.detail, name='detail'),
+    path('new/', views.create_article, name='create_article'),
+    path('<pk>/', views.detail, name='detail'),
+    path('update/<pk>/', views.update_article, name='update_article'),
+    path('delete/<pk>/', views.delete_article, name='delete_article'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact')
 ]
--- src/article/views.py
+++ src/article/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
 from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 def home(request):
@@ -15,6 +16,28 @@ def detail(request, pk):
         'article' : article
     }
     return render(request, 'detail.html', context)
+
+def create_article(request):
+    form = ArticleForm(request.POST or None)
+    if form.is_valid():
+       form.save()
+       return redirect('home')  #Here home = name of url
+    return render(request, 'article_form.html', {'form': form})
+
+def update_article(request, pk):
+    article = Article.objects.get(pk=pk)
+    form = ArticleForm(request.POST or None, instance=article)
+    if form.is_valid():
+       form.save()
+       return redirect('home')  #Here home = name of url
+    return render(request, 'article_form.html', {'form': form, 'article': article})
+
+def delete_article(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+       article.delete()
+       return redirect('home')  #Here home = name of url
+    return render(request, 'article_del_conf.html', {'article': article})
 
 def about(request):
     return render(request, 'about.html', {})
